145153bdce535ae83c31d2f1c685eed0e256596e
John Pacific <john.pacific@eventboard.io>
2016-12-12 13:14:42 -0700

    Get sean lists functioning

--- sean/sean.py
+++ sean/sean.py
@@ -5,94 +5,45 @@ import warnings
 
 from faker import Faker
 
-
-def seanify(sean_formatted_json):
-    factorized_json = {}
-
-    sean_json = json.loads(sean_formatted_json)
-    for key, value in sean_json.items():
-        try:
-            factorized_json[key] = factorize(value)
-        except ValueError as e:
-            raise e
-    return factorized_json
+from seantype import SeanType
+from handlers import DEFAULT_HANDLERS
 
 
-def factorize(value):
-    if type(value) == list:
-        return _factorize_values(_val=value)
-    elif type(value) == dict:
-        return _factorize_values(**value)
-    
+class Sean(object):
+    def __init__(self, sean_formatted_json, override_handlers={}):
+        self.sean_json = json.loads(sean_formatted_json)
+        self.handlers = DEFAULT_HANDLERS
+        self.handlers.update(override_handlers)
+        self.sean_types = self.seanify(sean_json=self.sean_json)
+        
+    def seanify(self, sean_json=None):
+        sean_data = (sean_json or self.sean_types)
+        data = self._handle_data(sean_data)
+        return data
 
-def factorize_string(length, frmt):
-    if not length:
-        raise ValueError("Invalid length for type string")
-    value = _gen_string(length)
-    if frmt:
-        value = frmt.format(value)
-    return value
-
-
-def factorize_int(length, frmt):
-    if not length:
-        raise ValueError("Invalid length for type int")
-    value = _gen_int(length)
-    if frmt:
-        value = frmt.format(value)
-    return value
-
-
-def factorize_dict(sean_val):
-    factor_dict = {}
-    for key, value in sean_val.items():
-        val = factorize(value)
-        factor_dict[key] = val
-    return factor_dict
-
-
-def factorize_list(sean_val, length=0):
-    factor_list = []
-    for i in range(1, length+1):
-        val = factorize(sean_val)
-        factor_list.append(val)
-    return factor_list
-
-
-def _factorize_values(_type=None, _len=0, _format=None, _val=None):
-    fake = Faker()
-
-    if not _type:
-        return random.choice(_val)
-    elif _type == 'string':
-        return factorize_string(_len, _format)
-    elif _type == 'int':
-        return factorize_int(_len, _format)
-    elif _type == 'dict':
-        return factorize_dict(_val)
-    elif _type == 'list':
-        return factorize_list(_val, _len)
-    elif _type == 'bool':
-        return fake.boolean()
-    elif _type == 'text':
-        return fake.text()
-    elif _type == 'sentence':
-        return fake.sentence()
-    elif _type == 'name':
-        return fake.name()
-    elif _type == 'email':
-        return fake.email()
-    elif _type == 'timestamp':
-        return int(fake.date_time_this_month().strftime("%s"))
-    else:
-        warnings.warn("Keyword not implemented - '{}'".format(_type),
-            SyntaxWarning)
-
-
-def _gen_string(size):
-    ascii_chars = string.ascii_letters + "0123456789"
-    return ''.join(random.choice(ascii_chars) for i in range(size))
-
-
-def _gen_int(size):
-    return random.randint(10**(size-1), (10**size)-1)
+    def _handle_data(self, sean_data):
+        data = {}
+        for key, value in sean_data.items():
+            if isinstance(value, SeanType):
+                data[key] = value.exec_handler()
+            elif isinstance(value, dict):
+                _type = value.get('_type', None)
+                if _type == 'dict':
+                    data[key] = self._handle_data(value['_val'])
+                elif _type == 'list':
+                    data_list = []
+                    for i in range(0, value.get('_len', 1)):
+                        data_list.append(self._handle_data(value['_val']))
+                    data[key] = data_list
+                elif _type:
+                    data[key] = SeanType(self.handlers[_type], **value)
+            elif isinstance(value, list):
+                # This prevents a Sean 'list' from being overwritten.
+                if not isinstance(value[0], dict):
+                    data[key] = SeanType(self.handlers[None], _val=value)
+                else:
+                    data_list = []
+                    for idx, val in enumerate(value):
+                        data_list.append(self._handle_data(val))
+                    data[key] = data_list
+        return data
--- sean/seantype.py
+++ sean/seantype.py
@@ -1,6 +1,9 @@
 class SeanType(object):
-    def __init__(self, **kwargs):
-        # A SeanType always has a type and a value, even None.
-        self._type = kwargs.get('_type', None)
+    def __init__(self, handler_func, **kwargs):
+        self.handler_func = handler_func
 
+        self._type = kwargs.get('_type', None)
         self.params = kwargs
+
+    def exec_handler(self):
+        return self.handler_func(**self.params)
